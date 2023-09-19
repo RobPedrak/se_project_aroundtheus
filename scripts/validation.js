9148d6eb7ee4e8600f820e80c5fd14f6880b2051
RobPedrak <RobPedrak@gmail.com>
2023-09-19 11:27:53 -0400

    feat: form validation (req. refactoring)

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,36 +1,77 @@
-// enabling validation by calling enableValidation()
-// pass all the settings on call
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    return showInputError(formEl, inputEl, options);
+  }
+  hideInputError(formEl, inputEl, options);
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
+}
+// move 31-33 into it's own 2 functions
+
+// disableButton
+//  add class if disabled
+// enableButton
+// remove class if enabled
+
+function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+  if (hasInvalidInput(inputEls)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
+  }
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(".modal__button");
+
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputEls, submitButton, options);
+    });
+  });
+}
 
 function enableValidation(options) {
-  const formEls = [...document.querySelectorAll(".modal__form")];
-  // const formEls = [...document.querySelectorAll(options.formSelector)];
+  const formEls = [...document.querySelectorAll(options.formSelector)];
   formEls.forEach((formEl) => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
     });
 
-    // look for all inputs inside of form
-    // loop through all inputs to see if all are valid
-    // if input is not valid
-    // get validation message
-    // add error class to input and make it red
-    // display error message
-    // disable button
-    // if all inputs are valid
-    // enable button
-    // reset error messages
+    setEventListeners(formEl, options);
   });
 }
 
 const config = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  formSelector: ".modal__form",
+  inputSelector: ".form__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "form__input_error",
+  errorClass: "form__error_visible",
 };
 
 enableValidation(config);
 
-// min 24
+// both forms' heights change when modal__error added by function
